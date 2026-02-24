bf8c1668ea504f4e660d5ea93eb0fcab59515c1b
ngabzar <ngabzar@gmail.com>
2026-02-25 05:44:24 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./index.tsx",
-    "./src/**/*.{html,ts}",
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
-}
+};
